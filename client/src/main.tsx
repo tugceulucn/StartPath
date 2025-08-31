a445d71529497719f3f9e8346a6c1fcf5d278cff
tugceulucn <fatmatugcelcn@gmail.com>
2025-08-31 15:55:13 +0300

    Created project

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
 
 
 createRoot(document.getElementById('root')!).render(
